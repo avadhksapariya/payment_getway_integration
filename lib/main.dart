f62d153a060d711a03cc446530b62870544829ec
ETAvadh <developer.empiere@gmail.com>
2025-02-12 10:27:17 +0530

    - Refactored the app name and package name with other relevant changes.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Payment Getway',
+      title: 'Payment Gateway',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
